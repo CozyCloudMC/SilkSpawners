b5ed542a0e3a7c8b60d6596bb8e8ef5afeb51215
Nick Huntley <nicholash2000@icloud.com>
2022-05-01 15:13:35 -0400

    Tab-completion support

--- src/me/CozyCloud/SilkSpawners/SilkSpawners.java
+++ src/me/CozyCloud/SilkSpawners/SilkSpawners.java
@@ -1,54 +1,14 @@
 package me.CozyCloud.SilkSpawners;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SilkSpawners extends JavaPlugin implements Listener {
+public class SilkSpawners extends JavaPlugin {
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new Events(), this);
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-        if (cmd.getName().equalsIgnoreCase("spawner") && sender instanceof Player player) {
-
-            if (args.length >= 2) {
-
-                if (args[0].equalsIgnoreCase("create")) {
-
-                    String types = "";
-                    for (EntityType type : EntityType.values()) types = types + type.toString() + ", ";
-                    if (!types.isEmpty()) types = types.substring(0, types.length()-2);
-
-                    try {
-                        EntityType type = EntityType.valueOf(args[1].toUpperCase());
-                        player.getInventory().addItem(SpawnerUtils.getSpawner(type));
-                        sender.sendMessage("Given " + SpawnerUtils.getSpawnerName(type));
-                    } catch (IllegalArgumentException e) {
-                        sender.sendMessage(ChatColor.RED + "Valid spawner types: " + types);
-                    }
-
-                }
-
-                else sender.sendMessage(ChatColor.RED + "Usage: /spawner create <name>");
-
-            }
-
-            else sender.sendMessage(ChatColor.RED + "Usage: /spawner create <name>");
-
-        }
-
-        return true;
-        
+        getCommand("spawner").setExecutor(new SpawnerCommand());
     }
 
 }
---
+++ src/me/CozyCloud/SilkSpawners/SpawnerCommand.java
@@ -0,0 +1,76 @@
+package me.CozyCloud.SilkSpawners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpawnerCommand implements TabExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (cmd.getName().equalsIgnoreCase("spawner") && sender instanceof Player player) {
+
+            String usage = ChatColor.RED + "Usage: /spawner create <name>";
+
+            if (args.length >= 2) {
+
+                if (args[0].equalsIgnoreCase("create")) {
+
+                    try {
+                        EntityType type = EntityType.valueOf(args[1].toUpperCase());
+                        player.getInventory().addItem(SpawnerUtils.getSpawner(type));
+                        sender.sendMessage("Given " + SpawnerUtils.getSpawnerName(type));
+                    } catch (IllegalArgumentException e) {
+                        sender.sendMessage(ChatColor.DARK_RED + args[1].toUpperCase() + ChatColor.RED + " is an invalid type!");
+                    }
+
+                }
+
+                else sender.sendMessage(usage);
+
+            }
+
+            else sender.sendMessage(usage);
+
+        }
+
+        return true;
+
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+
+        List<String> result = new ArrayList<String>();
+
+        if (cmd.getName().equalsIgnoreCase("spawner")) {
+
+            switch (args.length) {
+
+                case 1:
+                    result.add("create");
+                    break;
+
+                case 2:
+                    if (args[0].equalsIgnoreCase("create")) for (EntityType type : EntityType.values()) {
+                        String name = type.toString();
+                        if (name.toLowerCase().startsWith(args[1].toLowerCase())) result.add(name); //Only shows types that start with the user's entry
+                    }
+                    break;
+
+            }
+
+        }
+
+        return result;
+
+    }
+
+}
